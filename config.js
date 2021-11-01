41fa58f5bd7966d4765754cd6482f170b28b452d
AnantArora <shwetaaroragzb@gmail.com>
2021-11-01 10:41:22 +0530

    pro 77

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyCRlzfqesV3EpgN8nQrvThqWVUVC-L619M",
+    authDomain: "team-voting-app-1.firebaseapp.com",
+    databaseURL: "https://team-voting-app-1-default-rtdb.firebaseio.com",
+    projectId: "team-voting-app-1",
+    storageBucket: "team-voting-app-1.appspot.com",
+    messagingSenderId: "224611010050",
+    appId: "1:224611010050:web:91297fca49852f87fb491b"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
